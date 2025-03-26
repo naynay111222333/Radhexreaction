8c31b4886d34c04dca4e47b18a899747eb4fe847
naynay111222333 <tunlinnay1234@gmail.com>
2025-03-26 13:53:53 +0630

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "24072444"
+API_ID = "25779435"
 # -------------------------------------------------------------
-API_HASH = "580e1a3e7d0719816feebf595c9166d2"
+API_HASH = ""b4c6f5a3bf48c807f49c8803d303d68f
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "6949414014"))
-SUPPORT_GRP = "BOT_SUPPORT_GROUP7"
-UPDATE_CHNL = "ll_BOTCHAMBER_ll"
-OWNER_USERNAME = "RADHE_XD"
+OWNER_ID = int(getenv("OWNER_ID", "7429056115"))
+SUPPORT_GRP = "gittahlaing_chat"
+UPDATE_CHNL = "gittahlaing_ch"
+OWNER_USERNAME = "naynay11122211"
     

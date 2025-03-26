494d19f41a9fbb8f6c712c5089a6df5b13d5ce8d
naynay111222333 <tunlinnay1234@gmail.com>
2025-03-26 14:00:25 +0630

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ API_ID = "25779435"
 # -------------------------------------------------------------
 API_HASH = ""b4c6f5a3bf48c807f49c8803d303d68f
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-MONGO_URL = getenv("MONGO_URL", None)
+BOT_TOKEN = getenv("BOT_TOKEN", "8101426432:AAHPzfhYItgQ2Jo_yKNBIdmI24kf1QyZItI")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://babata:babata@babata.pk0yk.mongodb.net/?retryWrites=true&w=majority")
 OWNER_ID = int(getenv("OWNER_ID", "7429056115"))
 SUPPORT_GRP = "gittahlaing_chat"
 UPDATE_CHNL = "gittahlaing_ch"
